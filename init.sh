cc5cee1e93e352d2c22ef1003acc8a4b1d46d5e0
elghoto <ignaciobermudez@gmail.com>
2020-06-30 08:14:18 +0100

    Added init.sh script and configurations template

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source pirate-hole.conf
+rm -f docker-compose.yml
+envsubst <"docker-compose.yml.template"> "docker-compose.yml"
+
--- run-vpn.sh
+++ run-vpn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source vpn.conf
+source pirate-hole.conf
 
 docker run --rm -ti --cap-add=NET_ADMIN --cap-add=SYS_MODULE --device /dev/net/tun --name vpn \
 	--sysctl net.ipv4.conf.all.rp_filter=2 \
-	-e USER=$user -e PASS="$pass" \
+	-e USER=$VPNUSER -e PASS="$VPNPASS" \
 	-e CONNECT=us -e TECHNOLOGY=NordLynx -d bubuntux/nordvpn
