90a77f028335b376371dc6a2d7ba8874aef7f819
elghoto <ignaciobermudez@gmail.com>
2020-07-05 09:48:54 +0100

    whitelist subnets in vpn, and pick your server

--- run-vpn.sh
+++ run-vpn.sh
@@ -9,4 +9,4 @@ fi
 docker run --rm -ti --cap-add=NET_ADMIN --cap-add=SYS_MODULE --device /dev/net/tun --name vpn \
 	--sysctl net.ipv4.conf.all.rp_filter=2 \
 	-e USER=$VPNUSER -e PASS="$VPNPASS" \
-	-e CONNECT=us -e TECHNOLOGY=NordLynx $WHITELIST -d bubuntux/nordvpn
+	-e CONNECT=$VPNSERVER -e TECHNOLOGY=NordLynx $WHITELIST -d bubuntux/nordvpn
