6374d0bcf38675d9cac8eac018d2a5545f8063ee
elghoto <ignaciobermudez@gmail.com>
2020-08-12 22:12:58 +0100

    decreased health status intervals

--- run-vpn.sh
+++ run-vpn.sh
@@ -6,7 +6,8 @@ if [ -n $HOST_NET ]; then
 	WHITELIST="-e NETWORK=$HOST_NET"
 fi
 
-docker run --rm -ti --cap-add=NET_ADMIN --cap-add=SYS_MODULE --device /dev/net/tun --name vpn \
+docker run -ti --cap-add=NET_ADMIN --cap-add=SYS_MODULE --device /dev/net/tun --restart unless-stopped --name vpn \
 	--sysctl net.ipv4.conf.all.rp_filter=2 \
+	--health-timeout=30s --health-retries=10 --health-interval=30s \
 	-e USER=$VPNUSER -e PASS="$VPNPASS" \
 	-e CONNECT=$VPNSERVER -e TECHNOLOGY=NordLynx $WHITELIST -d bubuntux/nordvpn
